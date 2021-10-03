1692793ca3f267a57f546255a6ce2319aa8f9fdf
changnet <940477049@qq.com>
2021-10-03 13:57:30 +0800

    optimize

--- lancher.bat
+++ lancher.bat
@@ -1,8 +1,8 @@
 @echo off
 
-python reader.py --input ./ --srv server/ --clt client/ --timeout -1 --suffix .xlsx --swriter lua --cwriter json
+python exceltools.py --input ./ --srv server/ --clt client/ --timeout -1 --suffix .xlsx --swriter lua --cwriter json
 
-python reader.py --input ./ --srv server/ --clt client/ --timeout -1 --suffix .xlsx --cwriter lua
-python reader.py --input ./ --srv server/ --clt client/ --timeout -1 --suffix .xlsx --cwriter xml
+python exceltools.py --input ./ --srv server/ --clt client/ --timeout -1 --suffix .xlsx --cwriter lua
+python exceltools.py --input ./ --srv server/ --clt client/ --timeout -1 --suffix .xlsx --cwriter xml
 
 pause
--- lancher.sh
+++ lancher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 reader.py --input ./ --srv server/ --clt client/ --timeout -1 --suffix .xlsx --swriter lua --cwriter json
-python3 reader.py --input ./ --srv server/ --clt client/ --timeout -1 --suffix .xlsx --cwriter xml
-python3 reader.py --input ./ --srv server/ --clt client/ --timeout -1 --suffix .xlsx --cwriter lua
+python3 exceltools.py --input ./ --srv server/ --clt client/ --timeout -1 --suffix .xlsx --swriter lua --cwriter json
+python3 exceltools.py --input ./ --srv server/ --clt client/ --timeout -1 --suffix .xlsx --cwriter xml
+python3 reaexceltoolsder.py --input ./ --srv server/ --clt client/ --timeout -1 --suffix .xlsx --cwriter lua
 
